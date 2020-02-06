74de0b2882ffd24bc3a9574c8300d2b2c33ad42a
pr-dhruv <pra_mahendra@hotmail.com>
2020-02-06 15:47:11 +0530

    Completed first example

--- src/soma/java/regex/FirstProgram.java
+++ src/soma/java/regex/FirstProgram.java
@@ -10,19 +10,28 @@ public class FirstProgram {
     public static void main(String[] args) {
 	    String string= "abbabbba";
         int count = 0;
-	    // First We need to compile the pattern into Java that will return a Pattern object for that regex
+        /**
+         * First We need to compile the pattern into Java that will return a Pattern object for that regex
+         */
         Pattern pattern = Pattern.compile("ab");
 
         /**
          * Now we are going to find the pattern in the string, it is going to return the matcher patter
          * Using this matcher patter we are going to iterate all the matches.
-          */
+         */
         Matcher matcher = pattern.matcher(string);
 
-
+        /**
+         * matcher.find() => returns true if pattern is found otherwise it return false
+         */
         while(matcher.find()) {
             count++;
-            System.out.println(matcher.start());
+            /**
+             * matcher.start()  => return the starting index of the pattern
+             * matcher.end()    => return the ending index of the pattern
+             * matcher.group()  => return the string which matches the pattern
+             */
+            System.out.println(matcher.start() + "->" + matcher.end() + " => " + matcher.group());
         }
 
         System.out.println("Count : " + count);
