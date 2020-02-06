2312d8f01a611d1b4ade6e7671776a7d587afe1b
pr-dhruv <pra_mahendra@hotmail.com>
2020-02-06 15:38:23 +0530

    Comments added into the first example

--- src/soma/java/regex/FirstProgram.java
+++ src/soma/java/regex/FirstProgram.java
@@ -1,13 +1,30 @@
 package soma.java.regex;
 
+import com.sun.xml.internal.ws.addressing.WsaActionUtil;
+
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FirstProgram {
 
     public static void main(String[] args) {
 	    String string= "abbabbba";
-
+        int count = 0;
 	    // First We need to compile the pattern into Java that will return a Pattern object for that regex
         Pattern pattern = Pattern.compile("ab");
+
+        /**
+         * Now we are going to find the pattern in the string, it is going to return the matcher patter
+         * Using this matcher patter we are going to iterate all the matches.
+          */
+        Matcher matcher = pattern.matcher(string);
+
+
+        while(matcher.find()) {
+            count++;
+            System.out.println(matcher.start());
+        }
+
+        System.out.println("Count : " + count);
     }
 }
